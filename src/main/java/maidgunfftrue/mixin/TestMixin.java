79ece901dd4a4098b7cc7fd0e0840d5876c3f434
岩田 海斗 <ItohKaito@protonmail.com>
2026-07-01 18:00:44 +0900

    動かない-2

---
+++ src/main/java/maidgunfftrue/mixin/TestMixin.java
@@ -0,0 +1,21 @@
+package maidgunfftrue.mixin;
+
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public class TestMixin {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void useTest(Level level, Player player, InteractionHand usedHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+        player.kill();
+    }
+}
