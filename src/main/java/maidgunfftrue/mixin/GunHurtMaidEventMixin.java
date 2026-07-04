125ee05fad65aca914734c79117c23fa6643b5c1
岩田 海斗 <ItohKaito@protonmail.com>
2026-07-05 01:53:25 +0900

    Mixin その4 なんでだめなんだ…

--- src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
+++ src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
@@ -3,12 +3,15 @@ package maidgunfftrue.mixin;
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidHurtEvent;
 import com.github.tartaricacid.touhoulittlemaid.compat.gun.swarfare.event.GunHurtMaidEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GunHurtMaidEvent.class,remap = false)
 public class GunHurtMaidEventMixin {
-    @Overwrite
-    public void onMaidHurt(MaidHurtEvent event) {
+    @Inject(method = "onMaidHurt", at = @At("HEAD"), cancellable = true)
+    void CancelonMaidHurt(MaidHurtEvent event, CallbackInfo ci) {
         System.out.println("§b[MIXIN] onMaidHurt");
+        ci.cancel();
     }
 }
