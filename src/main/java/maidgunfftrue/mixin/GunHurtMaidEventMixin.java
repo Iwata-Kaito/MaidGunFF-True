8926ff94ef8b75bbff502c2149b1af6ce7c75397
岩田 海斗 <ItohKaito@protonmail.com>
2026-07-05 00:15:45 +0900

    Mixin その3

--- src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
+++ src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
@@ -2,37 +2,13 @@ package maidgunfftrue.mixin;
 
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidHurtEvent;
 import com.github.tartaricacid.touhoulittlemaid.compat.gun.swarfare.event.GunHurtMaidEvent;
-import com.tacz.guns.api.event.common.EntityHurtByGunEvent;
-import net.minecraft.world.damagesource.DamageSource;
-import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
-import net.neoforged.neoforge.event.level.ExplosionEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(GunHurtMaidEvent.class)
+@Mixin(value = GunHurtMaidEvent.class,remap = false)
 public class GunHurtMaidEventMixin {
     @Overwrite
     public void onMaidHurt(MaidHurtEvent event) {
         System.out.println("§b[MIXIN] onMaidHurt");
-    }
-
-    @Overwrite
-    public void onGunHurt(EntityHurtByGunEvent.Pre event) {
-        System.out.println("§b[MIXIN] onGunHurt");
-    }
-
-    @Overwrite
-    public void onPlayerHurt(LivingIncomingDamageEvent event) {
-        System.out.println("§b[MIXIN] onPlayerHurt");
-    }
-
-    @Overwrite
-    public void onExplosionDetonateEvent(ExplosionEvent.Detonate event) {
-        System.out.println("§b[MIXIN] onExplosionDetonateEvent");
-    }
-
-    @Overwrite
-    private boolean isBulletDamage(DamageSource source) {
-        return false;
     }
 }
