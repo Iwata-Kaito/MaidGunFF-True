86c8e3f9e83d286533697d0e446fbe64f4e69cea
岩田 海斗 <ItohKaito@protonmail.com>
2026-07-05 18:11:29 +0900

    Mixin その5 まだだめ…

--- src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
+++ src/main/java/maidgunfftrue/mixin/GunHurtMaidEventMixin.java
@@ -3,13 +3,14 @@ package maidgunfftrue.mixin;
 import com.github.tartaricacid.touhoulittlemaid.compat.gun.swarfare.event.GunHurtMaidEvent;
 import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = GunHurtMaidEvent.class,remap = false)
 public class GunHurtMaidEventMixin {
-    @Overwrite
-    private boolean isBulletDamage(DamageSource source) {
-        System.out.println("§e[MIXIN] isBulletDamage");
-        return false;   // 常に false を返す
+    @Inject(method = "isBulletDamage", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void isBulletDamage(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
     }
 }
